46cf2fce7407ae242ae106b758724a331c116b9f
Cry <i@mfzhuravlev.ru>
2025-05-14 08:44:15 +0300

    Added convenient output of ussies in the form of a table

--- cmd/list.go
+++ cmd/list.go
@@ -3,7 +3,9 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"text/tabwriter"
 
+	"github.com/fatih/color"
 	"github.com/google/go-github/v52/github"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -21,16 +23,39 @@ var listCmd = &cobra.Command{
 		opts := &github.IssueListByRepoOptions{
 			State: state,
 		}
-		issues, _, err := client.Issues.ListByRepo(ctx, owner, repo, opts)
 
+		items, _, err := client.Issues.ListByRepo(ctx, owner, repo, opts)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error fetching issues: %v\n", err)
 			os.Exit(1)
 		}
 
-		for _, issue := range issues {
-			fmt.Printf("#%d %s [%s]\n", issue.GetNumber(), issue.GetTitle(), issue.GetState())
+		if len(items) == 0 {
+			msg := fmt.Sprintf("No issues found for '%s/%s' with state='%s'", owner, repo, state)
+			fmt.Println(msg)
+			return
 		}
+
+		w := tabwriter.NewWriter(os.Stdout, 0, 2, 2, ' ', 0)
+		fmt.Fprintln(w, "Number\tState\tTitle")
+		for _, issue := range items {
+			// Цветное состояние
+			st := issue.GetState()
+			var stCol string
+			if st == "open" {
+				stCol = color.New(color.FgGreen).Sprint("● OPEN")
+			} else {
+				stCol = color.New(color.FgRed).Sprint("● CLOSED")
+			}
+
+			// Выводим: метки в квадратных скобках
+			fmt.Fprintf(w, "#%d\t%s\t%s\n",
+				issue.GetNumber(),
+				stCol,
+				issue.GetTitle(),
+			)
+		}
+		w.Flush()
 	},
 }
 
